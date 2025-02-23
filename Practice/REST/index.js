00f5e3f617979e32f623cfeebf8988ccd146308c
TanyaChaudhary010704 <punch010704@gmail.com>
2025-02-23 17:02:07 +0530

    REST

---
+++ Practice/REST/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+  {
+    id: uuidv4(),
+    username: "Tanya",
+    content: "Hey! I am learning REST",
+  },
+  {
+    id: uuidv4(),
+    username: "Apna College",
+    content: "We teacch millions of students",
+  },
+  {
+    id: uuidv4(),
+    username: "Tanisha",
+    content: "I am a passionate problem solver",
+  },
+  {
+    id: uuidv4(),
+    username: "Riya",
+    content: "I am great speaker",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("Server working well");
+});
+
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/form", (req, res) => {
+  res.render("form.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => p.id === id);
+  res.render("show.ejs", { post });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port : ${port}`);
+});
