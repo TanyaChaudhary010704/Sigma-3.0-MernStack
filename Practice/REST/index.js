82354f906c7b4533df5a854d0d9fdfc53982e980
TanyaChaudhary010704 <punch010704@gmail.com>
2025-02-23 17:58:31 +0530

    REST

--- Practice/REST/index.js
+++ Practice/REST/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const methodOverride = require("method-override");
 const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -59,6 +61,24 @@ app.get("/posts/:id", (req, res) => {
   res.render("show.ejs", { post });
 });
 
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => p.id === id);
+  post.content = newContent;
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  res.render("edit.ejs", { post });
+});
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => id != p.id);
+  res.redirect("/posts");
+});
 app.listen(port, () => {
   console.log(`Listening on port : ${port}`);
 });
