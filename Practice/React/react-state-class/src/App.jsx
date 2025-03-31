62f443525a2f5a086ee52cde716af921ea4e413c
TanyaChaudhary010704 <punch010704@gmail.com>
2025-03-31 21:16:21 +0530

    use State and Object

--- Practice/React/react-state-class/src/App.jsx
+++ Practice/React/react-state-class/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Counter from "./Counter";
 import LikeButton from "./LikeButton";
+import LudoBoard from "./LudoBoard";
 
 const App = () => {
   return (
     <>
-      <h1>States in React</h1>
+      {/* <h1>States in React</h1>
       <Counter />
-      <LikeButton/>
+      <LikeButton/> */}
+      <LudoBoard />
     </>
   )
 }
